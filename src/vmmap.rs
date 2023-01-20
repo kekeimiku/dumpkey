fbb7a20f46156b2c9ab62a76e1b9f941ce37d7f1
kekeimiku <kekelanact@gmail.com>
2023-01-20 16:25:43 +0800

    cleanup

--- src/vmmap.rs
+++ src/vmmap.rs
@@ -1,4 +1,4 @@
-#[allow(non_camel_case_types, non_snake_case, unused)]
+#![allow(non_camel_case_types, non_snake_case, clippy::not_unsafe_ptr_arg_deref)]
 pub mod ffi {
     pub type natural_t = ::core::ffi::c_uint;
     pub type integer_t = ::core::ffi::c_int;
@@ -171,12 +171,6 @@ pub mod ffi {
             pid: ::core::ffi::c_int,
             tn: *mut mach_port_name_t,
         ) -> kern_return_t;
-        pub fn proc_regionfilename(
-            pid: ::core::ffi::c_int,
-            address: u64,
-            buffer: *mut ::core::ffi::c_void,
-            buffersize: u32,
-        ) -> ::core::ffi::c_int;
         pub fn mach_vm_region(
             target_task: vm_task_entry_t,
             address: *mut mach_vm_address_t,
@@ -199,13 +193,6 @@ pub mod ffi {
             data: mach_vm_address_t,
             outsize: *mut mach_vm_size_t,
         ) -> kern_return_t;
-        pub fn mach_vm_write(
-            target_task: vm_map_t,
-            address: mach_vm_address_t,
-            data: vm_offset_t,
-            dataCnt: mach_msg_type_number_t,
-        ) -> kern_return_t;
-        pub fn strlen(cs: *const core::ffi::c_char) -> libc_size_t;
     }
 
     #[allow(clippy::missing_safety_doc)] // FIXME
@@ -218,7 +205,7 @@ use core::mem;
 
 use self::ffi::{
     dyld_all_image_infos, dyld_image_info, kern_return_t, mach_header_64, mach_vm_address_t, mach_vm_size_t,
-    segment_command_64, task_dyld_info,
+    segment_command_64,
 };
 
 // macos 64 bit only
@@ -237,15 +224,6 @@ pub fn mach_vm_read_overwrite(
     Ok(())
 }
 
-pub fn mach_vm_write(task: ffi::vm_task_entry_t, addr: u64, buf: &[u8]) -> Result<(), ffi::kern_return_t> {
-    let result: ffi::kern_return_t =
-        unsafe { ffi::mach_vm_write(task, addr, buf.as_ptr() as _, buf.len() as _) };
-    if result != ffi::KERN_SUCCESS {
-        return Err(result);
-    }
-    Ok(())
-}
-
 pub fn task_for_pid(pid: i32) -> Result<ffi::mach_port_name_t, ffi::kern_return_t> {
     let mut task: ffi::mach_port_name_t = ffi::MACH_PORT_NULL;
     let result = unsafe { ffi::task_for_pid(ffi::mach_task_self(), pid, &mut task) };
@@ -254,46 +232,6 @@ pub fn task_for_pid(pid: i32) -> Result<ffi::mach_port_name_t, ffi::kern_return_
     }
 
     Ok(task)
-}
-
-pub fn proc_regionfilename(pid: i32, address: u64) -> Option<Vec<u8>> {
-    let mut buf: Vec<u8> = Vec::with_capacity((ffi::PROC_PIDPATHINFO_MAXSIZE - 1) as _);
-    let ptr = buf.as_mut_ptr() as *mut ::core::ffi::c_void;
-    let size = buf.capacity() as u32;
-
-    let result: ffi::kern_return_t = unsafe { ffi::proc_regionfilename(pid, address, ptr, size) };
-
-    if result <= 0 {
-        None
-    } else {
-        unsafe {
-            buf.set_len(result as _);
-        }
-        Some(buf)
-    }
-}
-
-pub fn task_dyld_info(task: ffi::mach_port_name_t) -> Result<ffi::task_dyld_info, ffi::kern_return_t> {
-    const TASK_DYLD_INFO_COUNT: ffi::mach_msg_type_number_t = (mem::size_of::<ffi::task_dyld_info>()
-        / mem::size_of::<ffi::natural_t>())
-        as ffi::mach_msg_type_number_t;
-
-    let mut count = TASK_DYLD_INFO_COUNT;
-    let mut dyld_info = unsafe { mem::zeroed::<ffi::task_dyld_info>() };
-    let result: ffi::kern_return_t = unsafe {
-        ffi::task_info(
-            task,
-            ffi::TASK_DYLD_INFO,
-            &mut dyld_info as *mut ffi::task_dyld_info as ffi::task_info_t,
-            &mut count,
-        )
-    };
-
-    if result != ffi::KERN_SUCCESS {
-        Err(result)
-    } else {
-        Ok(dyld_info)
-    }
 }
 
 pub fn mach_vm_region(
@@ -308,101 +246,21 @@ pub fn mach_vm_region(
     unsafe { ffi::mach_vm_region(target_task, address, size, flavor, info, info_cnt, object_name) }
 }
 
-// TODO Return similar linux-style maps? `start-end perm pathname/dyld`
-#[allow(unused)]
-pub struct Map {
-    pub(crate) addr: ffi::mach_vm_address_t,
-    pub(crate) size: ffi::mach_vm_size_t,
-    pub(crate) count: ffi::mach_msg_type_number_t,
-    pub(crate) info: ffi::vm_region_extended_info,
-}
+pub fn get_offset(task: ffi::mach_port_name_t) -> Result<u64, kern_return_t> {
+    const TASK_DYLD_INFO_COUNT: ffi::mach_msg_type_number_t = (mem::size_of::<ffi::task_dyld_info>()
+        / mem::size_of::<ffi::natural_t>())
+        as ffi::mach_msg_type_number_t;
 
-pub struct MapIter {
-    task: ffi::vm_task_entry_t,
-    addr: ffi::mach_vm_address_t,
-}
+    let mut count = TASK_DYLD_INFO_COUNT;
+    let mut dyld_info = unsafe { mem::zeroed::<ffi::task_dyld_info>() };
+    let result: ffi::kern_return_t = unsafe {
+        ffi::task_info(task, ffi::TASK_DYLD_INFO, &mut dyld_info as *mut ffi::task_dyld_info as _, &mut count)
+    };
 
-impl MapIter {
-    pub const fn new(task: ffi::mach_port_name_t) -> Self {
-        Self { task, addr: 1 }
-    }
-}
-
-impl Default for MapIter {
-    fn default() -> Self {
-        Self { task: unsafe { ffi::mach_task_self() }, addr: 1 }
-    }
-}
-
-// TODO Maybe shouldnot use iterator?
-impl Iterator for MapIter {
-    type Item = Map;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut count = mem::size_of::<ffi::vm_region_extended_info_data_t>() as ffi::mach_msg_type_number_t;
-        let mut object_name: ffi::mach_port_t = 0;
-        let mut size = unsafe { mem::zeroed::<ffi::mach_vm_size_t>() };
-        let mut info = unsafe { mem::zeroed::<ffi::vm_region_extended_info_data_t>() };
-        let result: ffi::kern_return_t = mach_vm_region(
-            self.task,
-            &mut self.addr,
-            &mut size,
-            ffi::VM_REGION_EXTENDED_INFO,
-            &mut info as *mut ffi::vm_region_extended_info_data_t as ffi::vm_region_info_t,
-            &mut count,
-            &mut object_name,
-        );
-
-        if result != ffi::KERN_SUCCESS {
-            return None;
-        }
-        let region = Map { addr: self.addr, size, count, info };
-        self.addr += region.size;
-        Some(region)
-    }
-}
-
-impl Map {
-    pub const fn start(&self) -> usize {
-        self.addr as _
+    if result != ffi::KERN_SUCCESS {
+        return Err(result);
     }
 
-    pub const fn end(&self) -> usize {
-        (self.addr + self.size) as _
-    }
-
-    pub const fn size(&self) -> usize {
-        self.size as _
-    }
-
-    pub const fn is_read(&self) -> bool {
-        self.info.protection & ffi::VM_PROT_READ != 0
-    }
-
-    pub const fn is_write(&self) -> bool {
-        self.info.protection & ffi::VM_PROT_WRITE != 0
-    }
-
-    pub const fn is_exec(&self) -> bool {
-        self.info.protection & ffi::VM_PROT_EXECUTE != 0
-    }
-
-    pub const fn tag(&self) -> u32 {
-        self.info.user_tag
-    }
-}
-
-pub struct DyldInfo {
-    pub filename: String,
-    pub address: usize,
-    pub file_mod_date: usize,
-    pub segment: segment_command_64,
-}
-
-pub fn get_dyld_all_image_infos(
-    task: ffi::mach_port_name_t,
-    dyld_info: task_dyld_info,
-) -> Result<dyld_all_image_infos, kern_return_t> {
     let mut image_infos = unsafe { core::mem::zeroed::<ffi::dyld_all_image_infos>() };
     let mut read_len = std::mem::size_of_val(&image_infos) as mach_vm_size_t;
 
@@ -410,114 +268,46 @@ pub fn get_dyld_all_image_infos(
         task,
         dyld_info.all_image_info_addr,
         read_len,
-        (&mut image_infos) as *mut dyld_all_image_infos as mach_vm_address_t,
+        &mut image_infos as *mut dyld_all_image_infos as _,
         &mut read_len,
     )?;
-    Ok(image_infos)
-}
 
-pub fn get_dyld_image_info(
-    task: ffi::mach_port_name_t,
-    image_infos: dyld_all_image_infos,
-) -> Result<Vec<dyld_image_info>, kern_return_t> {
-    let mut modules =
-        vec![unsafe { core::mem::zeroed::<ffi::dyld_image_info>() }; image_infos.infoArrayCount as usize];
-    let mut read_len =
-        (core::mem::size_of::<dyld_image_info>() * image_infos.infoArrayCount as usize) as mach_vm_size_t;
+    // 只需要第一个
+    let mut module = unsafe { core::mem::zeroed::<ffi::dyld_image_info>() };
+    let mut read_len = (core::mem::size_of::<dyld_image_info>()) as mach_vm_size_t;
     mach_vm_read_overwrite(
         task,
         image_infos.infoArray as mach_vm_address_t,
         read_len,
-        modules.as_mut_ptr() as mach_vm_address_t,
+        &mut module as *mut ffi::dyld_image_info as _,
         &mut read_len,
     )?;
-    Ok(modules)
-}
 
-pub fn get_image_filename(
-    task: ffi::mach_port_name_t,
-    module: dyld_image_info,
-) -> Result<[i8; 512], kern_return_t> {
-    let mut read_len = 512_u64;
-    let mut image_filename = [0_i8; 512];
-    mach_vm_read_overwrite(
-        task,
-        module.imageFilePath as mach_vm_address_t,
-        read_len,
-        image_filename.as_mut_ptr() as mach_vm_address_t,
-        &mut read_len,
-    )?;
-    Ok(image_filename)
-}
-
-pub fn get_mach_header_64(
-    task: ffi::mach_port_name_t,
-    module: dyld_image_info,
-) -> Result<mach_header_64, kern_return_t> {
     let mut header = unsafe { core::mem::zeroed::<ffi::mach_header_64>() };
     let mut read_len = std::mem::size_of_val(&header) as mach_vm_size_t;
-
     mach_vm_read_overwrite(
         task,
         module.imageLoadAddress as u64,
         read_len,
-        (&mut header) as *mut mach_header_64 as mach_vm_address_t,
+        &mut header as *mut mach_header_64 as mach_vm_address_t,
         &mut read_len,
     )?;
-    Ok(header)
-}
 
-pub fn get_commands_buffer(
-    task: ffi::mach_port_name_t,
-    module: dyld_image_info,
-    header: mach_header_64,
-) -> Result<Vec<i8>, kern_return_t> {
     let mut commands_buffer = vec![0_i8; header.sizeofcmds as usize];
     let mut read_len = mach_vm_size_t::from(header.sizeofcmds);
     mach_vm_read_overwrite(
         task,
-        (module.imageLoadAddress as usize + core::mem::size_of_val(&header)) as mach_vm_size_t,
+        (module.imageLoadAddress as usize + core::mem::size_of_val(&header)) as _,
         read_len,
         commands_buffer.as_mut_ptr() as mach_vm_address_t,
         &mut read_len,
     )?;
-    Ok(commands_buffer)
-}
 
-pub fn get_offset(task: ffi::mach_port_name_t) -> Result<u64, kern_return_t> {
-    let dyld_info = task_dyld_info(task)?;
-    let image_infos = get_dyld_all_image_infos(task, dyld_info)?;
-    let modules = get_dyld_image_info(task, image_infos)?;
+    let command = unsafe { *(commands_buffer.as_ptr() as *const segment_command_64) };
+    let offset = module.imageLoadAddress as u64
+        - unsafe { *(commands_buffer.as_ptr().offset(command.cmdsize as _) as *const segment_command_64) }
+            .vmaddr
+        + command.vmaddr;
 
-    for module in modules {
-        let header = get_mach_header_64(task, module)?;
-        let commands_buffer = get_commands_buffer(task, module, header)?;
-        let mut offset: u32 = 0;
-        let mut slide: u64 = 0;
-        for _ in 0..header.ncmds {
-            unsafe {
-                let command =
-                    *(commands_buffer.as_ptr().offset(offset as isize) as *const segment_command_64);
-                if command.cmd == 0x19 && command.segname[0..7] == [95, 95, 84, 69, 88, 84, 0] {
-                    slide = module.imageLoadAddress as u64 - command.vmaddr;
-                    break;
-                }
-                offset += command.cmdsize;
-            }
-        }
-
-        let mut offset: u32 = 0;
-        for _ in 0..header.ncmds {
-            unsafe {
-                let mut command =
-                    *(commands_buffer.as_ptr().offset(offset as isize) as *const segment_command_64);
-                if command.cmd == 0x19 {
-                    command.vmaddr += slide;
-                    return Ok(command.vmaddr);
-                }
-                offset += command.cmdsize;
-            }
-        }
-    }
-    Err(0)
+    Ok(offset)
 }
