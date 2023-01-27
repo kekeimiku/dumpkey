0bb96d7636e0e1480d2746a0476609843724e3af
kekeimiku <kekelanact@gmail.com>
2023-01-27 22:39:22 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use dumpkey::{bytes_to_usize, Process};
 
 fn main() -> dumpkey::Result<()> {
     let pid = std::env::args().nth(1).ok_or(())?.parse()?;
-    let process = Process::open(pid)?;
-    let offset = process.get_offset()? as usize;
+    let proc = Process::open(pid)?;
+    let offset = proc.get_offset()? as usize;
 
+    // 0x105705c90 + 0 > 0x600001460ae0 + 8 > 0x60000170c560 + 16 > 0x60000170c5a0 + 64 > 6000026ac5e0
     let mut buf = vec![0; 8];
     let offset = 0x105705c90 + offset;
-    process.read_at_into(offset, buf.as_mut_slice())?;
+    proc.read_at_into(offset, buf.as_mut_slice())?;
     let offset = bytes_to_usize(&buf)? + 8;
-    process.read_at_into(offset, buf.as_mut_slice())?;
+    proc.read_at_into(offset, buf.as_mut_slice())?;
     let offset = bytes_to_usize(&buf)? + 16;
-    process.read_at_into(offset, buf.as_mut_slice())?;
+    proc.read_at_into(offset, buf.as_mut_slice())?;
     let offset = bytes_to_usize(&buf)? + 64;
 
+    println!("{:#x}",offset);
     let mut buf = vec![0; 32];
-    process.read_at_into(offset, buf.as_mut_slice())?;
+    proc.read_at_into(offset, buf.as_mut_slice())?;
 
     println!("key = 0x{}", buf.iter().map(|x| format!("{x:02x}")).collect::<String>());
 
