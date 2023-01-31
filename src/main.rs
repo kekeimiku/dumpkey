689ccfb190e533edc43c9fe9caf3b167d28f345b
kekeimiku <kekelanact@gmail.com>
2023-01-31 13:02:24 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> dumpkey::Result<()> {
     let proc = Process::open(pid)?;
     let offset = proc.get_offset()? as usize;
 
-    // 0x105705c90 + 0 > 0x600001460ae0 + 8 > 0x60000170c560 + 16 > 0x60000170c5a0 + 64 > 6000026ac5e0
+    // 0x105705c90->0->8->16->64->6000026ac5e0
     let mut buf = vec![0; 8];
     let offset = 0x105705c90 + offset;
     proc.read_at_into(offset, buf.as_mut_slice())?;
